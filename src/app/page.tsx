fb301e14cd90ed552753c889c9e8ebabcdc066ba
huseyinozay <huseyinozay27@gmail.com>
2023-07-26 13:55:52 +0300

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,18 +42,6 @@ export default function Home() {
 
       <div className={styles.grid}>
         <Link href={"detail"}>Detail</Link>
-        <Link href={"reports"}>Reports</Link>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -67,6 +55,7 @@ export default function Home() {
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
 
+        <Link href={"reports"}>Reports</Link>
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "../page.module.css";
 
-export default function Detail() {
+export default function Reports() {
   return <main className={styles.main}>Bir takım güzellikler...</main>;
 }
