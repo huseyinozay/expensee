e3f51d8d733f00b146bfc7e16e37426429fb2b0b
huseyinozay <huseyinozay27@gmail.com>
2023-07-27 14:37:25 +0300

    added requests

---
+++ src/apiService.ts
@@ -0,0 +1,91 @@
+type Method = "GET" | "POST" | "PUT" | "DELETE";
+
+class ApiService {
+  private baseUrl: string | undefined;
+  private token: string | null = null;
+
+  constructor() {
+    this.baseUrl = "https://api.masraff.co/";
+  }
+
+  public setToken(token: string): void {
+    this.token = token;
+  }
+
+  private async request<T>(
+    url: string,
+    method: Method,
+    body?: any,
+    params?: any
+  ): Promise<T> {
+    const options: RequestInit = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (localStorage.getItem("access_token")) {
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      };
+    }
+
+    if (params) {
+      url = `${url}?${toQueryString(params)}`;
+    }
+
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+    if (body && method === "POST" && url === "token") {
+      options.body = new URLSearchParams(body);
+    }
+
+    const response = await fetch(this.baseUrl + url, options);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // If no content returned (204 status), resolve the promise with null
+    if (response.status === 204) return null as T;
+
+    const data = await response.json();
+    return data;
+  }
+
+  public get<T>(url: string, query?: any): Promise<T> {
+    return this.request<T>(url, "GET", undefined, query);
+  }
+
+  public post<T>(url: string, data: any): Promise<T> {
+    return this.request<T>(url, "POST", data);
+  }
+
+  public update<T>(url: string, data: any): Promise<T> {
+    return this.request<T>(url, "PUT", data);
+  }
+
+  public delete<T>(url: string): Promise<T> {
+    return this.request<T>(url, "DELETE");
+  }
+}
+
+function toQueryString(params: any): string {
+  return Object.entries(params)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value
+          .map(
+            (v) => `${encodeURIComponent(key)}=${encodeURIComponent(String(v))}`
+          )
+          .join("&");
+      }
+      return `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`;
+    })
+    .join("&");
+}
+
+export default ApiService;
--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,6 +1,50 @@
+"use client";
+
 import Image from "next/image";
 import styles from "../page.module.css";
+import ApiService from "@/apiService";
+import { useEffect, useState } from "react";
+
+const api = new ApiService();
 
 export default function Reports() {
-  return <main className={styles.main}>Bir takım güzellikler...</main>;
+  const authData = {
+    grant_type: "password",
+    username: "huseyin.ozay@masraff.co",
+    password: 145236,
+    client_id: "adminApp",
+  };
+  const filter = {
+    includes: "user,approver,subCompany,user.company",
+    clientId: "adminApp",
+    page: 0,
+    pageSize: 10,
+    ascending: "false",
+    searchTypeId: 0,
+  };
+  const [token, setToken] = useState();
+  async function logIn() {
+    const response = await api.post("token", authData);
+    console.log(response);
+    //@ts-ignore
+    setToken(response.access_token);
+    return response;
+  }
+
+  async function getReports() {
+    const response = await api.get("v1/reports/getDefaultReports", filter);
+    console.log(response);
+  }
+
+  useEffect(() => {
+    //@ts-ignore
+    localStorage.setItem("access_token", token);
+  }, [token]);
+  return (
+    <main className={styles.main}>
+      Bir takım güzellikler...
+      <button onClick={logIn}>login</button>
+      <button onClick={getReports}>getReports</button>
+    </main>
+  );
 }
