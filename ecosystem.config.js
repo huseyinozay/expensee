533a55b1c8d45cd4e4f0b6857b752afdee90d1b5
huseyinozay <huseyinozay27@gmail.com>
2023-07-27 11:20:28 +0300

    port changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "expensee",
       script: "./node_modules/next/dist/bin/next",
-      args: "start -p " + 3000,
+      args: "start -p " + 8080,
       watch: false,
       autorestart: true,
     },
